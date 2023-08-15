79b1ccb909df803ac292d8e055eab18ebcaf4c04
mitverma <amit.wohlig@gmail.com>
2023-08-15 14:24:46 +0530

    mongodb connection to the new cluster

--- config/config-dto.ts
+++ config/config-dto.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   PORT: '3000',
-  DB_CONNECTION_STRING: 'mongodb://localhost:27017/btc',
+  DB_CONNECTION_STRING: 'mongodb+srv://amitwohlig:Zoro@9594@cluster0.si5nno7.mongodb.net/',
   SWAGGER: 'true',
   DETAILED_VALIDATION_MESSAGE: 'showValidationInDetail',
   ENVIRONMENT: 'development',
--- config/env-vars-config.ts
+++ config/env-vars-config.ts
@@ -2,7 +2,7 @@ import { CONFIG } from './config-dto';
 
 export default () => ({
   [CONFIG.PORT]: parseInt(process.env.PORT, 10) || 3000,
-  [CONFIG.DB_CONNECTION_STRING]: process.env.DB_CONNECTION_STRING,
+  [CONFIG.DB_CONNECTION_STRING]: process.env.DB_CONNECTION_STRING || 'mongodb+srv://amitwohlig:Zoro@9594@cluster0.si5nno7.mongodb.net/',
   [CONFIG.SWAGGER]: process.env.ENABLE_SWAGGER,
   [CONFIG.DETAILED_VALIDATION_MESSAGE]: process.env.DETAILED_VALIDATION_MESSAGE,
   [CONFIG.ENVIRONMENT]: process.env.ENVIRONMENT,
