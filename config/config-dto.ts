a10f4bcf6e68b7a473e9857dca9bb8fe83c73237
mitverma <amit.wohlig@gmail.com>
2023-09-24 11:35:54 +0530

    cluster url changes

--- config/config-dto.ts
+++ config/config-dto.ts
@@ -1,7 +1,8 @@
 export const CONFIG = {
   PORT: '3000',
   // DB_CONNECTION_STRING: 'mongodb://localhost:27017/taxi-back',
-  DB_CONNECTION_STRING: 'mongodb+srv://amitwohlig:Zoro@9594@cluster0.si5nno7.mongodb.net/',
+  // DB_CONNECTION_STRING: 'mongodb+srv://amitwohlig:Zoro@9594@cluster0.si5nno7.mongodb.net/',
+  DB_CONNECTION_STRING: 'mongodb+srv://abhidevelopment8225:taxiback999@taxiback.9osskuh.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
   SWAGGER: 'true',
   DETAILED_VALIDATION_MESSAGE: 'showValidationInDetail',
   ENVIRONMENT: 'development',
